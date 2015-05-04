7875e41ff231e270071865d4d1cca479c501e099
Synergy <kaechelv@hochschule-trier.de>
2015-05-04 18:31:37 +0200

    Grafik-Branch

--- core/src/de/project/ice/IceGame.java
+++ core/src/de/project/ice/IceGame.java
@@ -6,6 +6,9 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+/**
+ * Grafik Branch
+ */
 public class IceGame extends ApplicationAdapter {
     SpriteBatch batch;
     Texture img;
