6e052a9c4ffa886a610ee1bb0ae1963076437a26
Marco Kirchner <kirchnermarco@gmail.com>
2016-03-04 15:47:01 +0100

    screw you update 4

--- core/src/de/project/ice/ecs/Components.kt
+++ core/src/de/project/ice/ecs/Components.kt
@@ -1,26 +1,24 @@
 package de.project.ice.ecs
 
-import com.badlogic.ashley.core.ComponentMapper
-import com.badlogic.ashley.core.Entity
 import de.project.ice.ecs.components.*
 
 object Components {
-    val animation = ComponentMapper.getFor(AnimationComponent::class.java)
-    val camera = ComponentMapper.getFor(CameraComponent::class.java)
-    val texture = ComponentMapper.getFor(TextureComponent::class.java)
-    @JvmField val transform = ComponentMapper.getFor(TransformComponent::class.java)
-    val disabled = ComponentMapper.getFor(DisabledComponent::class.java)
-    val script = ComponentMapper.getFor(ScriptComponent::class.java)
-    val control = ComponentMapper.getFor(ControlComponent::class.java)
-    val move = ComponentMapper.getFor(MoveComponent::class.java)
-    val breath = ComponentMapper.getFor(BreathComponent::class.java)
-    val name = ComponentMapper.getFor(NameComponent::class.java)
-    val hotspot = ComponentMapper.getFor(HotspotComponent::class.java)
-    val walkarea = ComponentMapper.getFor(WalkAreaComponent::class.java)
-    val use = ComponentMapper.getFor(UseComponent::class.java)
-    val pathplanning = ComponentMapper.getFor(PathPlanningComponent::class.java)
-    val walking = ComponentMapper.getFor(WalkingComponent::class.java)
-    val distanceScale = ComponentMapper.getFor(DistanceScaleComponent::class.java)
+    public val animation = ComponentMapper.getFor(AnimationComponent::class.java)
+    public val camera = ComponentMapper.getFor(CameraComponent::class.java)
+    public val texture = ComponentMapper.getFor(TextureComponent::class.java)
+    public val transform = ComponentMapper.getFor(TransformComponent::class.java)
+    public val disabled = ComponentMapper.getFor(DisabledComponent::class.java)
+    public val script = ComponentMapper.getFor(ScriptComponent::class.java)
+    public val control = ComponentMapper.getFor(ControlComponent::class.java)
+    public val move = ComponentMapper.getFor(MoveComponent::class.java)
+    public val breath = ComponentMapper.getFor(BreathComponent::class.java)
+    public val name = ComponentMapper.getFor(NameComponent::class.java)
+    public val hotspot = ComponentMapper.getFor(HotspotComponent::class.java)
+    public val walkarea = ComponentMapper.getFor(WalkAreaComponent::class.java)
+    public val use = ComponentMapper.getFor(UseComponent::class.java)
+    public val pathplanning = ComponentMapper.getFor(PathPlanningComponent::class.java)
+    public val walking = ComponentMapper.getFor(WalkingComponent::class.java)
+    public val distanceScale = ComponentMapper.getFor(DistanceScaleComponent::class.java)
 }
 
 fun Entity.hasComponent(mapper: ComponentMapper<IceComponent>): Boolean = mapper.has(this)
