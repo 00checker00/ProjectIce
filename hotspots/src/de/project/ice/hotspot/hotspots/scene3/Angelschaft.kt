b6a3ab8349c6173f669d64f678ca05e0c0fd74f5
Marco Kirchner <kirchnermarco@gmail.com>
2016-03-14 14:53:48 +0100

    Klassen für Szene 1 hinzugefügt

--- hotspots/src/de/project/ice/hotspot/hotspots/scene3/Angelschaft.kt
+++ hotspots/src/de/project/ice/hotspot/hotspots/scene3/Angelschaft.kt
@@ -4,8 +4,12 @@ import de.project.ice.IceGame
 import de.project.ice.hotspot.Use
 
 
-class Angelschaft : Use.Adapter() {
+class Angelschaft : Use.Take("Angelschaft","Angelschaft") {
     override fun look(game: IceGame) {
         game.showMessages("s3_shaft_desc");
+    }
+
+    override fun take(game: IceGame) {
+        super.take(game)
     }
 }
