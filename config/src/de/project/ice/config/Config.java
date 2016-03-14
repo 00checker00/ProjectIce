afb70b5f6dc0423e9511394c7f39559463a6f037
Marco Kirchner <kirchnermarco@gmail.com>
2016-03-14 11:57:18 +0100

    change hotspot key to space

--- config/src/de/project/ice/config/Config.java
+++ config/src/de/project/ice/config/Config.java
@@ -4,7 +4,7 @@ public abstract class Config
 {
     public static final float PIXELS_TO_METRES = 1.0f / 128.0f;
     public static final int INVENTORY_KEY = 37; // I key
-    public static final int HOTSPOT_KEY = 61; // Tab key
+    public static final int HOTSPOT_KEY = 62; // Space key
     public static final int MENU_KEY = 131; // ESC Key
     public static final boolean RENDER_DEBUG = true;
 }
