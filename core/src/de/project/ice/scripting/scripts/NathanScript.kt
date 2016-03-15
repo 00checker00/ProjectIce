ecb453b280dbea42872392f4eab2dcc4b0abd66f
Marco Kirchner <kirchnermarco@gmail.com>
2016-03-15 14:41:01 +0100

    add runOnce script util

--- core/src/de/project/ice/scripting/scripts/NathanScript.kt
+++ core/src/de/project/ice/scripting/scripts/NathanScript.kt
@@ -3,22 +3,20 @@ package de.project.ice.scripting.scripts
 import com.badlogic.ashley.core.Entity
 import de.project.ice.Storage
 import de.project.ice.scripting.Script
+import de.project.ice.scripting.runOnce
+import java.util.*
 
 /**
  * Created by Antoni on 04.03.2016.
  */
 class NathanScript: Script() {
-
-
     override fun onUpdateEntity(entity: Entity, delta: Float) {
         super.onUpdateEntity(entity, delta)
 
-        if(Storage.SAVESTATE.getString("s2_angelrute")=="true" && Storage.SAVESTATE.getBoolean("s2_angelrutebekommen")==false)
-        {
-            Game().inventory.addItem("inv_fishing_rod")
-            Storage.SAVESTATE.put("s2_angelrutebekommen",true)
+        if(Storage.SAVESTATE.getString("s2_angelrute")=="true") {
+            runOnce("s2_angelrutebekommen") {
+                Game().inventory.addItem("inv_fishing_rod")
+            }
         }
-
-
     }
 }
