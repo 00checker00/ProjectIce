2986bb8e6b8cb8d532a1f795856787812e5a3ef4
Philipp <schoenep@fh-trier.de>
2016-03-15 16:46:35 +0100

    merge origin master

--- hotspots/src/de/project/ice/hotspot/hotspots/scene2/Angelkuebel.kt
+++ hotspots/src/de/project/ice/hotspot/hotspots/scene2/Angelkuebel.kt
@@ -1,15 +1,23 @@
 package de.project.ice.hotspot.hotspots.scene2
 
 import de.project.ice.IceGame
+import de.project.ice.Storage
 import de.project.ice.hotspot.Use
+import de.project.ice.scripting.runOnce
 
 
-class Angelkuebel : Use.Adapter() {
+class Angelkuebel : Use.Take("", "inv_fishing_rod") {
     override fun look(game: IceGame) {
         game.showMessages("s2_fishing_rod_cylinder_desc");
     }
 
     override fun take(game: IceGame) {
-        game.showMessages("s2_fishing_rod_theft")
+
+        if(Storage.SAVESTATE.getString("s2_angelrute")=="true") {
+        //    super.take(game)
+        } else {
+            game.showMessages("s2_fishing_rod_theft")
+        }
+
     }
 }
--- hotspots/src/de/project/ice/hotspot/hotspots/scene2/Schneemann.kt
+++ hotspots/src/de/project/ice/hotspot/hotspots/scene2/Schneemann.kt
@@ -5,6 +5,10 @@ import de.project.ice.hotspot.Use
 
 
 class Schneemann : Use.Adapter() {
+    override fun speak(game: IceGame) {
+        game.showDialog("s2_snowman_talk_desc")
+    }
+
     override fun look(game: IceGame) {
         game.showMessages("s2_snowman_desc");
     }
