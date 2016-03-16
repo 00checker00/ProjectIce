93ec8165babc0c25d455b73f844225bb71bd3058
Marco Kirchner <kirchnermarco@gmail.com>
2016-03-16 15:10:28 +0100

    add move callback

--- core/src/de/project/ice/scripting/ScriptUtils.kt
+++ core/src/de/project/ice/scripting/ScriptUtils.kt
@@ -2,6 +2,7 @@ package de.project.ice.scripting
 
 import de.project.ice.IceGame
 import de.project.ice.Storage
+import java.util.concurrent.atomic.AtomicInteger
 
 fun runOnce(id: String, func: ()->Unit) {
     if(Storage.SAVESTATE.getBoolean("__ONLYONCE__${id}__")==false)
@@ -12,15 +13,22 @@ fun runOnce(id: String, func: ()->Unit) {
 }
 
 fun IceGame.blockInteraction(func: ()->Unit) {
-    val blocked = BlockInteraction
+    blockInteractionCounter.incrementAndGet()
     BlockInteraction = true
     func.invoke()
-    BlockInteraction = blocked
+    if(blockInteractionCounter.decrementAndGet() == 0) {
+        BlockInteraction = false
+    }
 }
 
 fun IceGame.blockSaving(func: ()->Unit) {
-    val blocked = BlockSaving
+    blockSavingCounter.incrementAndGet()
     BlockSaving = true
     func.invoke()
-    BlockSaving = blocked
+    if(blockSavingCounter.decrementAndGet() == 0) {
+        BlockSaving = false
+    }
 }
+
+internal val blockInteractionCounter = AtomicInteger(0)
+internal val blockSavingCounter = AtomicInteger(0)
