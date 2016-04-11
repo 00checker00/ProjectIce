99940183594eb39e9a8eb9c6c0b025ead42579c6
Marco Kirchner <kirchnermarco@gmail.com>
2016-04-11 14:14:17 +0200

    drag selected entity

--- editor/src/de/project/ice/editor/EditGameScreen.kt
+++ editor/src/de/project/ice/editor/EditGameScreen.kt
@@ -1,28 +1,23 @@
 package de.project.ice.editor
 
-import com.badlogic.ashley.core.Family
+import com.badlogic.ashley.core.Entity
 import com.badlogic.gdx.Input
 import com.badlogic.gdx.InputAdapter
 import com.badlogic.gdx.InputProcessor
 import com.badlogic.gdx.graphics.OrthographicCamera
-import com.badlogic.gdx.math.Rectangle
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.math.Vector3
-import com.badlogic.gdx.utils.Array
 import de.project.ice.config.Config.PIXELS_TO_METRES
 import de.project.ice.ecs.Components
 import de.project.ice.ecs.Families
-import de.project.ice.ecs.components.HotspotComponent
-import de.project.ice.ecs.components.TextureComponent
 import de.project.ice.ecs.components.TransformComponent
-import de.project.ice.ecs.hasComponent
 import de.project.ice.ecs.getComponent
-import de.project.ice.ecs.systems.RenderingSystem
 import de.project.ice.editor.undoredo.ModifySceneAction
 import de.project.ice.screens.BaseScreenAdapter
 
 class EditGameScreen(app: EditorApplication) : BaseScreenAdapter(app) {
     override val priority = 3
+    var selectedEntity: Entity? = null;
 
     override val inputProcessor: InputProcessor =  object : InputAdapter() {
         private var sceneRecording: ModifySceneAction.Recording? = null
@@ -55,45 +50,8 @@ class EditGameScreen(app: EditorApplication) : BaseScreenAdapter(app) {
                 val cameraComponent = Components.camera.get(activeCamera)
                 val coords = cameraComponent.camera.unproject(Vector3(screenX.toFloat(), screenY.toFloat(), 0f))
                 if (button == Input.Buttons.LEFT) {
-                    val entities = Array(app.engine.getEntitiesFor(
-                            Family.all(TransformComponent::class.java)
-                                  .one(TextureComponent::class.java, HotspotComponent::class.java)
-                            .get()).toArray())
-
-                    entities.sort(RenderingSystem.RenderingComparator())
-                    entities.reverse()
-                    for (entity in entities) {
-                        val transform = Components.transform.get(entity)
-
-                        val width: Float
-                        val height: Float
-
-                        if (entity.hasComponent(Components.hotspot)) {
-                            val hotspot = entity.getComponent(Components.hotspot)
-
-                            width = hotspot.width
-                            height = hotspot.height
-                        } else if (entity.hasComponent(Components.texture)) {
-
-                            val texture = Components.texture.get(entity)
-
-                            if (!texture.region.isValid) {
-                                continue
-                            }
-
-                            width = texture.region.data!!.regionWidth * PIXELS_TO_METRES
-                            height = texture.region.data!!.regionHeight * PIXELS_TO_METRES
-                        } else {
-                            continue
-                        }
-
-                        if (Rectangle(transform.pos.x-width/2, transform.pos.y, width, height).contains(coords.x, coords.y)) {
-                            dragComponent = transform
-                            dragOriginX = coords.x - transform.pos.x
-                            dragOriginY = coords.y - transform.pos.y
-                            return false
-                        }
-                    }
+                    dragOriginX = coords.x
+                    dragOriginY = coords.y
                     return false
                 } else if (button == Input.Buttons.MIDDLE) {
                     cameraDragDown.set(screenX.toFloat(), screenY.toFloat())
@@ -104,7 +62,7 @@ class EditGameScreen(app: EditorApplication) : BaseScreenAdapter(app) {
         }
 
         override fun touchDragged(screenX: Int, screenY: Int, pointer: Int): Boolean {
-            if (dragComponent != null) {
+            if (selectedEntity != null) {
                 val cameras = app.engine.getEntitiesFor(Families.camera)
                 if (cameras.size() == 0) {
                     return false
@@ -114,7 +72,14 @@ class EditGameScreen(app: EditorApplication) : BaseScreenAdapter(app) {
                 val cameraComponent = Components.camera.get(activeCamera)
                 val coords = cameraComponent.camera.unproject(Vector3(screenX.toFloat(), screenY.toFloat(), 0f))
 
-                dragComponent!!.pos.set(Vector2(coords.x - dragOriginX, coords.y - dragOriginY))
+                val dx = coords.x - dragOriginX
+                val dy = coords.y - dragOriginY
+
+                selectedEntity?.getComponent(Components.transform)?.pos?.add(dx, dy)
+
+                dragOriginX = coords.x
+                dragOriginY = coords.y
+
             } else if (cameraDrag != null) {
                 cameraDrag!!.translate(Vector2(screenX.toFloat(), screenY.toFloat()).sub(cameraDragDown).scl(PIXELS_TO_METRES).scl(-1f, 1f))
                 cameraDragDown.set(screenX.toFloat(), screenY.toFloat())
